b9d3f3f6b49958b7d91cfe814e6e2ce7944647f2
Pedro Santos <opedrosantos16@gmail.com>
2026-09-15 17:05:17 -0300

    feat: definição da classe FSMoves

--- src/05_fs_moves.py
+++ src/05_fs_moves.py
@@ -15,97 +15,116 @@ df_base = pd.read_sql("matches", con)
 df_base
 
 # %%
-def qtde_pieces(piece):
-    piece_name = chess.piece_name(piece)
-    turn = board.turn
 
-    match[board.ply()][f"qtde_{piece_name}"] = len(board.pieces(piece, turn))
+class FSMoves:
 
-def diff_piece(piece):
-    piece_name = chess.piece_name(piece)
-    turn = board.turn
-    opponent = not board.turn
+    def __init__(self, board, match):
+        self.board = board
+        self.match = match
+        
+    def qtde_pieces(self, piece):
+        piece_name = chess.piece_name(piece)
+        turn = self.board.turn
+        print(len(
+            self.board.pieces(piece, turn)))
 
-    qtde_pieces = len(board.pieces(piece, turn))
-    qtde_pieces_opponent = len(board.pieces(piece, opponent))
+        self.match[self.board.ply()][f"qtde_{piece_name}"] = len(
+            self.board.pieces(piece, turn))
 
-    match[board.ply()][f"diff_{piece_name}"] = qtde_pieces - qtde_pieces_opponent
+    def diff_piece(self, piece):
+        piece_name = chess.piece_name(piece)
+        turn = self.board.turn
+        opponent = not self.board.turn
 
-def has_castled():
-    if board.ply() >= 2:
-        castled = match[board.ply() - 2]["has_castled"]
-    else:
-        castled = False
+        qtde_pieces = len(self.board.pieces(piece, turn))
+        qtde_pieces_opponent = len(self.board.pieces(piece, opponent))
 
-    match[board.ply()]["has_castled"] = castled or board.is_castling(move)
+        self.match[self.board.ply()][f"diff_{piece_name}"] = qtde_pieces - qtde_pieces_opponent
+
+    def has_castled(self, move):
+        if self.board.ply() >= 2:
+            castled = self.match[self.board.ply() - 2]["has_castled"]
+        else:
+            castled = False
+
+        self.match[self.board.ply()]["has_castled"] = castled or self.board.is_castling(move)
+
+    def is_capture(self, move):
+        self.match[self.board.ply()]["is_capture"] = self.board.is_capture(move)
+
+    def move_piece(self, piece):
+        self.match[self.board.ply()]["move_piece"] = chess.piece_name(piece.piece_type)
+
+    def is_irreversible(self, move):
+        self.match[self.board.ply()]["is_irreversible"] = self.board.is_irreversible(move)
+
+    def is_in_check(self):
+        self.match[self.board.ply()]["is_in_check"] = self.board.is_check()
+
+    def gives_check(self, move):
+        self.match[self.board.ply()]["gives_check"] = self.board.gives_check(move)
+
+    def is_2_repetition(self):
+        self.match[self.board.ply()]["is_2_repetition"] = self.board.is_repetition(count=2)
+
+    def is_trade(self, move):
+        if self.board.is_capture(move) and (self.match[self.board.ply()-1]["is_capture"] == True):
+            self.match[self.board.ply()]["is_trade"] = self.board.is_capture(move)
+        else:
+            self.match[self.board.ply()]["is_trade"] = False
 
 
-# Fazer uma lista de dicionários, cada dicinoário sendo as features do lance. e no fim adicionar tudo no df
+def feature_store(pgn_str):
 
-# Se eu apagar a coluna turn, continuo sabendo exatamente o que esse número representa?
+    pgn = io.StringIO(pgn_str)
+    game = chess.pgn.read_game(pgn)
+    board = game.board()
 
-pgn = df_base.iloc[6]["moves"]
-pgn = io.StringIO(pgn)
+    match = []
+    fs_moves = FSMoves(board, match)
 
-game = chess.pgn.read_game(pgn)
-board = game.board()
-    
-match = []
+    for move in game.mainline_moves():
+        move_dict = {}
+        match.append(move_dict)
+        piece = board.piece_at(move.from_square)
+        turn = board.turn
 
-for move in game.mainline_moves():
-    move_dict = {}
-    match.append(move_dict)
-    piece = board.piece_at(move.from_square)
-    turn = board.turn
+        match[board.ply()]["move"] = move
 
-    match[board.ply()]["move"] = move
+        # features about de player
 
-    # features about de player
+        if turn == white:
+            match[board.ply()]["turn"] = "white"
 
-    if turn == white:
-        # white's turn
-        match[board.ply()]["turn"] = "white"
+        else:
+            match[board.ply()]["turn"] = "black"
 
-    else:
-        # black's turn
-        match[board.ply()]["turn"] = "black"
+        # features about the move
+        fs_moves.is_capture(move)
+        fs_moves.is_trade(move)
+        fs_moves.move_piece(piece)
+        fs_moves.is_irreversible(move)
+        fs_moves.is_in_check()
+        fs_moves.gives_check(move)
+        fs_moves.is_2_repetition()
 
-    # features históricas
+        # features históricas
+        for p in pieces[:-1]:
+            fs_moves.qtde_pieces(p)
+            fs_moves.diff_piece(p)
+        fs_moves.has_castled(move)
 
-    for p in pieces[:-1]:
-        qtde_pieces(p)
-        diff_piece(p)
-    has_castled()
+        board.push(move)
 
-    # features about the move
+    return match
 
-    # is_capture
-    match[board.ply()]["is_capture"] = board.is_capture(move)
 
-    # move_piece
-    match[board.ply()]["move_piece"] = chess.piece_name(piece.piece_type)
 
-    # is_irreversible
-    match[board.ply()]["is_irreversible"] = board.is_irreversible(move)
+pgn_str = df_base.iloc[6]["moves"]
 
-    # is_in_check
-    match[board.ply()]["is_in_check"] = board.is_check()
+features = feature_store(pgn_str)
 
-    # gives_check
-    match[board.ply()]["gives_check"] = board.gives_check(move)
-
-    # is_2_repetition
-    match[board.ply()]["is_2_repetition"] = board.is_repetition(count=2)
-
-    # is_trade
-    if board.is_capture(move) and (match[board.ply()-1]["is_capture"] == True):
-        match[board.ply()]["is_trade"] = board.is_capture(move)
-    else:
-        match[board.ply()]["is_trade"] = False
-
-    board.push(move)
-
-teste = pd.DataFrame(match)
+teste = pd.DataFrame(features)
 teste.head(30)
 
 # %%
@@ -114,70 +133,12 @@ df_base.iloc[6]
 
 # %%
 
-pgn
-# %%
 
-pgn = df_base.iloc[4]["moves"]
-pgn = io.StringIO(pgn)
+# print(board.halfmove_clock)
+#     número de half-moves desde a última captura ou movimento de peão
 
-game = chess.pgn.read_game(pgn)
-board = game.board()
+# attacks(square: chess.Square) → SquareSet
+# is_attacked_by(color: chess.Color, square: chess.Square, occupied: chess.IntoSquareSet | None = None) → bool
+# attackers(color: chess.Color, square: chess.Square, occupied: chess.IntoSquareSet | None = None) → SquareSet
+# print(board.is_pinned(True, square))
 
-for move in game.mainline_moves():
-    # print(board.halfmove_clock)
-        # número de half-moves desde a última captura ou movimento de peão
-    # print(board.promoted.bit_count())
-    # print(board.is_check())
-        # Tests if the current side to move is in check.
-    # print(board.gives_check(move))
-    if board.is_repetition(count=2):
-        print("sim")
-        break
-        # repetiu duas vezes?
-    # print(board.is_capture(move))
-    # print(board.is_zeroing(move))
-    #     captura ou lance de peão
-    # print(board.is_irreversible(move))
-    #     pawn moves, captures, moves that destroy castling rights and moves that cede en passant are irreversible
-    # print(board.is_castling(move))
-    # print(board.is_kingside_castling(move))
-    # print(board.is_queenside_castling(move))
-
-    # branco
-    # print(board.has_castling_rights(True))
-    # print(board.has_kingside_castling_rights(True))
-    # print(board.has_queenside_castling_rights(True))
-
-    # preto
-    # print(board.has_castling_rights(False))
-    # print(board.has_kingside_castling_rights(False))
-    # print(board.has_queenside_castling_rights(False))
-
-    # baseboard
-
-    # attacks(square: chess.Square) → SquareSet
-    # is_attacked_by(color: chess.Color, square: chess.Square, occupied: chess.IntoSquareSet | None = None) → bool
-    # attackers(color: chess.Color, square: chess.Square, occupied: chess.IntoSquareSet | None = None) → SquareSet
-
-    #qtd peças brancas
-    # print(len(board.pieces(color=True, piece_type=chess.PAWN)))
-    # print(len(board.pieces(color=True, piece_type=chess.KNIGHT)))
-    # print(len(board.pieces(color=True, piece_type=chess.BISHOP)))
-    # print(len(board.pieces(color=True, piece_type=chess.ROOK)))
-    # print(len(board.pieces(color=True, piece_type=chess.QUEEN)))
-
-    #qtd peças pretas
-    # print(len(board.pieces(color=False, piece_type=chess.PAWN)))
-    # print(len(board.pieces(color=False, piece_type=chess.KNIGHT)))
-    # print(len(board.pieces(color=False, piece_type=chess.BISHOP)))
-    # print(len(board.pieces(color=False, piece_type=chess.ROOK)))
-    # print(len(board.pieces(color=False, piece_type=chess.QUEEN)))
-
-
-    # for square in chess.SQUARES:
-    #     print(board.is_pinned(True, square))
-    # print(move)
-    board.push(move)
-
-# %%
-print(game.end().ply())
